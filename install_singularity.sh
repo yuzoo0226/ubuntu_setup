ce148c826303f2f2704611d884bd2163164b4f05
fryuzoo <fryuzoo@gmail.com>
2022-10-02 00:47:13 +0900

    inital commit

---
+++ install_singularity.sh
@@ -0,0 +1,28 @@
+sudo apt update
+
+sudo apt-get install -y \
+   build-essential \
+   libseccomp-dev \
+   pkg-config \
+   squashfs-tools \
+   cryptsetup
+   
+export VERSION=1.17.2 OS=linux ARCH=amd64
+wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
+rm go$VERSION.$OS-$ARCH.tar.gz
+echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+
+# install singularity
+export VERSION=3.9.5
+wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-ce-${VERSION}.tar.gz
+
+tar -xzf singularity-ce-${VERSION}.tar.gz
+cd singularity-ce-${VERSION}
+
+./mconfig && \
+make -C builddir && \
+sudo make -C builddir install
+
+
