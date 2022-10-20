e1ea82574f172f56199fa8e803567af24b766f23
yuzoo0226 <fryuzoo@gmail.com>
2022-10-21 02:06:43 +0900

    [add]QT install

---
+++ install/Qt.sh
@@ -0,0 +1,7 @@
+# Qt install
+sudo apt update && sudo apt upgrade -y
+sudo apt install qtbase5-dev qttools5-dev-tools qt5-default -y
+sudo apt install qtcreator -y
+
+# use opencv for C++
+sudo apt install libopencv-dev
