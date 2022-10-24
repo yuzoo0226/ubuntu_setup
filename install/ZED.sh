c7b44300cf54f07b22b2d3c58d4112ff0c69235d
fryuzoo <fryuzoo@gmail.com>
2022-10-24 16:07:27 +0900

    [add]ZED install script

---
+++ install/ZED.sh
@@ -0,0 +1,5 @@
+wget https://download.stereolabs.com/zedsdk/3.6/cu115/ubuntu18 -O ./ZED_SDK_Ubuntu18_cuda11.5_v3.6.2.run
+chmod +x ZED_SDK_Ubuntu18_cuda11.5_v3.6.2.run
+./ZED_SDK_Ubuntu18_cuda11.5_v3.6.2.run
+rm ZED_SDK_Ubuntu18_cuda11.5_v3.6.2.run
+# 選択肢はすべてデフォルト，yesを選択している
