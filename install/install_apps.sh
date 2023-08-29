8079f3fb12cfa052db6dfa11fb4e0ad579637d87
yuzoo0226 <fryuzoo@gmail.com>
2023-08-29 16:31:31 +0900

    modify anydesk

--- install/install_apps.sh
+++ install/install_apps.sh
@@ -41,7 +41,7 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams
 sudo apt update
 sudo apt install teams
 
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
 sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
 sudo apt update
 sudo apt install anydesk
