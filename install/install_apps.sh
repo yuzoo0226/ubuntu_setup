e956019eabde8b208b6f624047b738dfb03badc6
yuzoo0226 <fryuzoo@gmail.com>
2025-04-26 16:28:47 +0900

    delete some app

--- install/install_apps.sh
+++ install/install_apps.sh
@@ -1,14 +1,14 @@
 # install slack
-sudo snap install slack
+# sudo snap install slack
 
-# install vscode
-sudo snap install --classic code
+# # install vscode
+# sudo snap install --classic code
 
-# install google chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
-# google-chrome
+# # install google chrome
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb
+# rm google-chrome-stable_current_amd64.deb
+# # google-chrome
 
 #echo "install atom"
 #wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
@@ -23,11 +23,11 @@ sudo add-apt-repository ppa:stebbins/handbrake-releases
 sudo apt-get update
 sudo apt-get install handbrake -y
 
-echo " install texlive"
+# echo " install texlive"
 # sudo apt install texlive-full -y
 
-echo "install meshlab"
-sudo apt install meshlab -y
+# echo "install meshlab"
+# sudo apt install meshlab -y
 
 echo "install gimp"
 sudo apt install gimp -y
@@ -35,13 +35,13 @@ sudo apt install gimp -y
 echo "gnome tweaks"
 sudo apt install gnome-tweaks -y
 
-echo "install teams"
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/source>'
-sudo apt update
-sudo apt install teams
+# echo "install teams"
+# curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+# sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/source>'
+# sudo apt update
+# sudo apt install teams
 
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
-sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
-sudo apt update
-sudo apt install anydesk
+# wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+# sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+# sudo apt update
+# sudo apt install anydesk
